08bc096b21827f29d4454da0f276a2a17be72ce2
avagardiner <avamgardiner@gmail.com>
2024-05-05 16:07:21 +1000

    Uploaded Documents

---
+++ Assignment 2 - Programming Task/Assignment 2 - Instructions (Assessment Specifications)/AssignmentTemplate.sql
@@ -0,0 +1,83 @@
+-- Please rename this file to your StudentID_StudentName
+-- SECTION-ONE
+
+-- Task 1 
+/* Insert your codes in the correct order of execution. If Table A needs to be created before Table B, add codes in cthe orrect order */
+
+-- Table creation
+
+-- Data Insertion
+
+
+-- Task 2
+
+
+-- Task 3
+
+
+-- Task 4
+
+
+-- Task 5
+
+
+-- Task 6
+
+
+-- Task 7
+
+
+-- Task 8
+
+
+-- Task 9
+
+
+-- Task 10
+
+
+-- Task 11
+
+
+-- Task 12
+
+
+-- Task 13
+
+
+-- SECTION-TWO
+
+/* Insert table creation and data for populating the tables */
+
+
+-- Task 14
+
+
+-- Task 15
+
+
+
+-- SECTION-THREE
+
+/* Insert table creation and data for populating the tables */
+
+
+-- Task 16
+
+
+-- Task 17
+
+
+
+-- SECTION-FOUR
+
+/* Insert table creation and data for populating the tables */
+
+
+-- Task 18
+
+
+-- Task 19
+
+
+-- Task 20 
